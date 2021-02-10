1e1fb11649f5312e995c1f3eb9138c441c706dc5
unknown <upade.arfah18@siesgst.ac.in>
2021-02-10 14:35:20 +0530

    Added a comment

--- src/App.js
+++ src/App.js
@@ -1,43 +1,44 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
-import { useSelector,useDispatch } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import './App.css';
 
 import Chat from './Chat';
-import {selectUser,login,logout  } from "./features/userSlice";
+import { selectUser, login, logout } from "./features/userSlice";
 import Login from './Login';
 import { auth } from './firebase';
 function App() {
   const user = useSelector(selectUser);
-   const dispatch = useDispatch()
+  const dispatch = useDispatch()
   useEffect(() => {
     auth.onAuthStateChanged((authUser) => {
-     // console.log("user is ", authUser);
-      if(authUser){
-         dispatch(
-           login({
-             uid:authUser.uid,
-             photo:authUser.photoURL,
-             email:authUser.email,
-             displayName: authUser.displayName
-           })
-         )
-      }else{
+      // console.log("user is ", authUser);
+      // console.log("user is ", authUser);
+      if (authUser) {
+        dispatch(
+          login({
+            uid: authUser.uid,
+            photo: authUser.photoURL,
+            email: authUser.email,
+            displayName: authUser.displayName
+          })
+        )
+      } else {
         dispatch(logout())
       }
     })
   }, [dispatch])
   return (
 
-    <div className="app"> 
-    {user ? (
-      <>
-      <Chat />
-      </>
-    ):(
-     <Login />
-    )}
-      
+    <div className="app">
+      {user ? (
+        <>
+          <Chat />
+        </>
+      ) : (
+          <Login />
+        )}
+
     </div>
   );
 }
